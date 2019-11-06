ccfdc678016838f0c9c93107a7c696b253d05fb7
Alex Bonwit <alexbonwit@mac.com>
2019-11-06 22:37:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]["Assembly".push]
-
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+  
+  programmer_hash
 end
